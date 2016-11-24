8379f729c8f92763abe8ffef57536ffdc1e936c7
徐晔 <xuye@mmtrix.com>
2016-11-24 14:58:34 +0800

    update

--- tcpip/transport/mm/mm_packet_list.go
+++ tcpip/transport/mm/mm_packet_list.go
@@ -3,7 +3,7 @@
 // license that can be found in the LICENSE file.
 
 // Package ilist provides the implementation of intrusive linked lists.
-package udp
+package mm
 
 // List is an intrusive list. Entries can be added to or removed from the list
 // in O(1) time and with no additional memory allocations.
@@ -14,34 +14,34 @@ package udp
 //      for e := l.Front(); e != nil; e = e.Next() {
 // 		// do something with e.
 //      }
-type udpPacketList struct {
-	head *udpPacket
-	tail *udpPacket
+type mmPacketList struct {
+	head *mmPacket
+	tail *mmPacket
 }
 
 // Reset resets list l to the empty state.
-func (l *udpPacketList) Reset() {
+func (l *mmPacketList) Reset() {
 	l.head = nil
 	l.tail = nil
 }
 
 // Empty returns true iff the list is empty.
-func (l *udpPacketList) Empty() bool {
+func (l *mmPacketList) Empty() bool {
 	return l.head == nil
 }
 
 // Front returns the first element of list l or nil.
-func (l *udpPacketList) Front() *udpPacket {
+func (l *mmPacketList) Front() *mmPacket {
 	return l.head
 }
 
 // Back returns the last element of list l or nil.
-func (l *udpPacketList) Back() *udpPacket {
+func (l *mmPacketList) Back() *mmPacket {
 	return l.tail
 }
 
 // PushFront inserts the element e at the front of list l.
-func (l *udpPacketList) PushFront(e *udpPacket) {
+func (l *mmPacketList) PushFront(e *mmPacket) {
 	e.SetNext(l.head)
 	e.SetPrev(nil)
 
@@ -55,7 +55,7 @@ func (l *udpPacketList) PushFront(e *udpPacket) {
 }
 
 // PushBack inserts the element e at the back of list l.
-func (l *udpPacketList) PushBack(e *udpPacket) {
+func (l *mmPacketList) PushBack(e *mmPacket) {
 	e.SetNext(nil)
 	e.SetPrev(l.tail)
 
@@ -69,7 +69,7 @@ func (l *udpPacketList) PushBack(e *udpPacket) {
 }
 
 // PushBackList inserts list m at the end of list l, emptying m.
-func (l *udpPacketList) PushBackList(m *udpPacketList) {
+func (l *mmPacketList) PushBackList(m *mmPacketList) {
 	if l.head == nil {
 		l.head = m.head
 		l.tail = m.tail
@@ -85,7 +85,7 @@ func (l *udpPacketList) PushBackList(m *udpPacketList) {
 }
 
 // InsertAfter inserts e after b.
-func (l *udpPacketList) InsertAfter(b, e *udpPacket) {
+func (l *mmPacketList) InsertAfter(b, e *mmPacket) {
 	a := b.Next()
 	e.SetNext(a)
 	e.SetPrev(b)
@@ -99,7 +99,7 @@ func (l *udpPacketList) InsertAfter(b, e *udpPacket) {
 }
 
 // InsertBefore inserts e before a.
-func (l *udpPacketList) InsertBefore(a, e *udpPacket) {
+func (l *mmPacketList) InsertBefore(a, e *mmPacket) {
 	b := a.Prev()
 	e.SetNext(a)
 	e.SetPrev(b)
@@ -113,7 +113,7 @@ func (l *udpPacketList) InsertBefore(a, e *udpPacket) {
 }
 
 // Remove removes e from l.
-func (l *udpPacketList) Remove(e *udpPacket) {
+func (l *mmPacketList) Remove(e *mmPacket) {
 	prev := e.Prev()
 	next := e.Next()
 
@@ -133,27 +133,27 @@ func (l *udpPacketList) Remove(e *udpPacket) {
 // Entry is a default implementation of Linker. Users can add anonymous fields
 // of this type to their structs to make them automatically implement the
 // methods needed by List.
-type udpPacketEntry struct {
-	next *udpPacket
-	prev *udpPacket
+type mmPacketEntry struct {
+	next *mmPacket
+	prev *mmPacket
 }
 
 // Next returns the entry that follows e in the list.
-func (e *udpPacketEntry) Next() *udpPacket {
+func (e *mmPacketEntry) Next() *mmPacket {
 	return e.next
 }
 
 // Prev returns the entry that precedes e in the list.
-func (e *udpPacketEntry) Prev() *udpPacket {
+func (e *mmPacketEntry) Prev() *mmPacket {
 	return e.prev
 }
 
 // SetNext assigns 'entry' as the entry that follows e in the list.
-func (e *udpPacketEntry) SetNext(entry *udpPacket) {
+func (e *mmPacketEntry) SetNext(entry *mmPacket) {
 	e.next = entry
 }
 
 // SetPrev assigns 'entry' as the entry that precedes e in the list.
-func (e *udpPacketEntry) SetPrev(entry *udpPacket) {
+func (e *mmPacketEntry) SetPrev(entry *mmPacket) {
 	e.prev = entry
 }
