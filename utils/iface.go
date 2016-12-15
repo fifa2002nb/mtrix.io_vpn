41bc764dfd6a971db59b50d161777cb65283d1cf
徐晔 <xuye@mmtrix.com>
2016-12-15 14:29:19 +0800

    update

--- utils/iface.go
+++ utils/iface.go
@@ -37,39 +37,74 @@ func SetTunIP(tunName string, subnetIP global.Address, subnetMask uint8) error {
 	args := strings.Split(sargs, " ")
 	cmd := exec.Command("ip", args...)
 	if err := cmd.Run(); nil != err {
-		return err
+		return errors.New(fmt.Sprintf("ip %v err:%v", sargs, err))
 	}
 
 	sargs = fmt.Sprintf("link set dev %s up mtu 1500 qlen 100", tunName)
 	args = strings.Split(sargs, " ")
 	cmd = exec.Command("ip", args...)
 	if err := cmd.Run(); nil != err {
-		return err
+		return errors.New(fmt.Sprintf("ip %v err:%v", sargs, err))
 	}
 
 	sargs = fmt.Sprintf("addr add dev %s local %s peer %s", tunName, ip, peer)
 	args = strings.Split(sargs, " ")
 	cmd = exec.Command("ip", args...)
 	if err := cmd.Run(); nil != err {
-		return err
+		return errors.New(fmt.Sprintf("ip %v err:%v", sargs, err))
 	}
 
 	sargs = fmt.Sprintf("route add %s via %s dev %s", subnet, peer, tunName)
 	args = strings.Split(sargs, " ")
 	cmd = exec.Command("ip", args...)
 	if err := cmd.Run(); nil != err {
-		return err
+		return errors.New(fmt.Sprintf("ip %v err:%v", sargs, err))
 	}
 	return nil
 }
 
 // ip tuntap del mode tun tun0
-func CleanTunIP(tunName string) error {
+func CleanTunIP(tunName string, subnetIP global.Address, subnetMask uint8) error {
+	ip, subnet, err := ParseCIDR(subnetIP, subnetMask)
+	if nil != err {
+		return err
+	}
+
+	ip = ip.To4()
+	if ip[3]%2 == 0 {
+		return errors.New("Invalid ip address.")
+	}
+
+	peer := net.IP(make([]byte, 4))
+	copy([]byte(peer), []byte(ip))
+	peer[3]++
+
+	sargs := fmt.Sprintf("route del %s via %s dev %s", subnet, peer, tunName)
+	args := strings.Split(sargs, " ")
+	cmd := exec.Command("ip", args...)
+	if err := cmd.Run(); nil != err {
+		return errors.New(fmt.Sprintf("ip %v err:%v", sargs, err))
+	}
+
+	sargs = fmt.Sprintf("addr del dev %s local %s peer %s", tunName, ip, peer)
+	args = strings.Split(sargs, " ")
+	cmd = exec.Command("ip", args...)
+	if err := cmd.Run(); nil != err {
+		return errors.New(fmt.Sprintf("ip %v err:%v", sargs, err))
+	}
+
+	sargs = fmt.Sprintf("link set %s down", tunName)
+	args = strings.Split(sargs, " ")
+	cmd = exec.Command("ip", args...)
+	if err := cmd.Run(); nil != err {
+		return errors.New(fmt.Sprintf("ip %v err:%v", sargs, err))
+	}
+
 	sargs := fmt.Sprintf("tuntap del mode tun %s", tunName)
 	args := strings.Split(sargs, " ")
 	cmd := exec.Command("ip", args...)
 	if err := cmd.Run(); nil != err {
-		return err
+		return errors.New(fmt.Sprintf("ip %v err:%v", sargs, err))
 	}
 	return nil
 }
